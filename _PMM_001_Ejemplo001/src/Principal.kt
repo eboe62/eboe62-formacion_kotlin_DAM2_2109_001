0c124564f67831a4f9a95eb0f55941853bc8f643
eboe62 <76263991+eboe62@users.noreply.github.com>
2022-11-06 00:03:30 +0100

    Add files via upload
    added Kotlin suma Ejemplo. issue#3

---
+++ _PMM_001_Ejemplo001/src/Principal.kt
@@ -0,0 +1,16 @@
+// Si no da la opción de Run application seguir las siguientes intrucciones
+// https://beginnersbook.com/2017/12/kotlin-in-eclipse-ide/
+// https://stackoverflow.com/questions/50885551/run-as-kotlin-application-missing-eclipse
+// https://stackoverflow.com/questions/64527844/cannot-access-java-io-serializable-which-is-a-supertype-of-kotlin-string-ch
+
+class Hello
+
+fun main() {
+val a = 5
+val b = 36
+val c = a + b
+	
+ println("sum of $a and $b is $c")
+ println("sum of $a and $b is ${a + b}")
+ println("sum of $a and $b is ${sumar(a,b)}")
+}
---
+++ _PMM_001_Ejemplo001/src/Suma.kt
@@ -0,0 +1,4 @@
+class suma 
+fun sumar(a: Int, b: Int): Int {
+	return a + b
+}
